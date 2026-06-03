0ca1fb2a1e4369dc3ce713d8b3c293364de27658
saidsakersandoval0219-commits <saidsakersandoval0219@gmail.com>
2026-06-03 09:01:06 -0500

    feat: :zap: inicio de la creacion de eventos del proecyto

---
+++ Data_Base_Funtions.sql
@@ -0,0 +1,52 @@
+-- 1) fn_CalcularTotalVenta: Calcula el monto total de una venta específica.
+
+DELIMITER $$
+
+CREATE FUNCTION	 fn_CalcularTotalVenta(
+	p_id_venta INT	
+)
+
+RETURNS DECIMAL(10,2)
+
+DETERMINISTIC
+
+BEGIN
+	DECLARE	 total  DECIMAL(10,2);
+	SELECT SUM(cantidad * precio_unitario_congelado) INTO total FROM detalle_ventas
+	WHERE id_venta = p_id_venta;
+
+	RETURN total;
+END $$
+
+DELIMITER ;
+
+SELECT fn_CalcularTotalVenta(1);
+
+-- 2) fn_VerificarDisponibilidadStock: Valida si hay stock suficiente para un producto.
+|	
+DELIMITER $$
+
+CREATE FUNCTION fn_VerificarDisponibilidadStock(
+    p_id_producto INT,
+    cantidad_solicitada INT
+)
+RETURNS BOOLEAN
+
+DETERMINISTIC
+
+BEGIN
+
+    DECLARE stockDisponible INT;
+
+    SELECT stock
+    INTO stockDisponible
+    FROM productos
+    WHERE id_producto = p_id_producto;
+
+    RETURN stockDisponible >= cantidad_solicitada;
+
+END $$
+
+DELIMITER ;
+
+SELECT fn_VerificarDisponibilidadStock(1, 5);
